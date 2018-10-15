209bd9d1d901023eb962726998a7fea3db6713e3
youngdot <sean.d.davis86@gmail.com>
2018-10-15 10:33:41 -0500

    First daily journal commit

---
+++ scripts/journal.js
@@ -0,0 +1,41 @@
+const journalEntries= [
+    {
+        date: "07/24/2018",
+        concept: "Array methods",
+        entry: "We learned about 4 different array methods today. forEach made sense, but the others still comsfuse me.",
+        mood: "ok"
+    }
+]
+
+/* 
+To create, and return, a string template that return 
+a single journal entry as HTML.
+*/
+
+const journalEntries = [
+    {
+        concept: "Array methods",
+        date: "07/25/2018",
+        entry: "We learned about array methods, but only forEach made sense",
+        mood: "Ok"
+    }
+] 
+
+const makeJournalEntryComponent = (journalEntry) => {
+    
+    return 
+}
+
+/* 
+Purpose: To render all journal entries to the DOM
+Arguments: entries (array of objects)
+*/
+
+const renderJournalEntreies = (entries) => {
+
+}
+
+//Invoke the render function
+renderJournalEntries (journalEntries)
+
+
